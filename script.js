acc8748f9baa5a67946eef09afaaf181c71edaba
Kevin Olivella <kevinolivella@gmail.com>
2024-10-27 00:06:04 -0500

    Se crea la función mostrarImpuesto, y se ejecuta para mostrar por consola

--- script.js
+++ script.js
@@ -15,22 +15,60 @@ function iniciarSesion() {
                 console.log('Contarseña incorrecta. Inténtelo de nuevo')
                 intentos--;
             }
-        }
+        } return true;
+    }else {
+        console.log('Usuario incorrecto')
+        return false;
     }
-} iniciarSesion();
+} 
 
 function capturarIngresoEmpleado() {
-    let ingresoMensualEMpleado = Number(prompt('Ingrese el sueldo del empleado: '))
-    var impuesto= 0
-    if (ingresoMensualEMpleado < 2000000) {
-         impuesto = ingresoMensualEMpleado * 0.1
-         console.log(impuesto)
-    } else if (ingresoMensualEMpleado >=2000000 && ingresoMensualEMpleado == 5000000){
-        impuesto = ingresoMensualEMpleado * 0.15
-        console.log(impuesto)
-    }else {
-        impuesto= ingresoMensualEMpleado * 0.2
-        console.log(impuesto)
+    let numeroEmpleados = true
+
+    while(numeroEmpleados){
+        let ingresoMensualEMpleado = Number(prompt('Ingrese el sueldo del empleado: '))
+        return ingresoMensualEMpleado;
     }
+    // var impuesto= 0
+    // if (ingresoMensualEMpleado < 2000000) {
+    //      impuesto = ingresoMensualEMpleado * 0.1
+    //      console.log(impuesto)
+    // } else if (ingresoMensualEMpleado >=2000000 && ingresoMensualEMpleado == 5000000){
+    //     impuesto = ingresoMensualEMpleado * 0.15
+    //     console.log(impuesto)
+    // }else {
+    //     impuesto= ingresoMensualEMpleado * 0.2
+    //     console.log(impuesto)
+    // }
     
-}capturarIngresoEmpleado()
+
+} 
+
+function calcularImpuesto(ingresoMensualEMpleado) {
+    
+    if (capturarIngresoEmpleado() < 2000000) {
+        return ingresoMensualEMpleado  * 0.10;
+    } else if (capturarIngresoEmpleado() > 2000000 && capturarIngresoEmpleado() < 5000000) {
+        return ingresoMensualEMpleado * 0.15;
+    } else {
+        return ingresoMensualEMpleado * 0.20;
+
+    } 
+}
+
+function mostrarImpuesto(){
+    console.log(`El impuesto sobre la renta a pagar es: ${impuesto}`)
+}
+
+if(iniciarSesion()){
+    let continuar = true;
+
+    while(continuar){
+        let ingreso = capturarIngresoEmpleado();
+        let impuesto = calcularImpuesto(ingresoMensualEMpleado);
+        mostrarImpuesto(impuesto);
+    }
+    console.log("Calculo realizado")
+} else{
+    console.log("Finalizado")
+}
